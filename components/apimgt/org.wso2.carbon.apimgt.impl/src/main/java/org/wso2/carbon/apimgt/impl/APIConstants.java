1cce69740ce5ee65dddb2b267f0d9c70724b8864
msm1992 <madusha.sachini@yahoo.com>
2021-09-30 14:59:27 +0530

    Add mediators to support query param and resource path operations

--- components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/APIConstants.java
+++ components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/APIConstants.java
@@ -2805,4 +2805,5 @@ public final class APIConstants {
     public static final String CONTENT_TYPE = "contentType";
     public static final String HEADERS = "headers";
     public static final String STATUS_CODE = "statusCode";
+    public static final String PATH_PARAM_PREFIX = "uri.var.";
 }
