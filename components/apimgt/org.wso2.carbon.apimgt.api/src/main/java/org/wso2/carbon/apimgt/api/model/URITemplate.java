771743eee48577458956b007cd418a07613f5be1
Sachini De Silva <madusha.sachini@yahoo.com>
2025-07-22 12:07:18 +0530

    Add changes to sort build issues

--- components/apimgt/org.wso2.carbon.apimgt.api/src/main/java/org/wso2/carbon/apimgt/api/model/URITemplate.java
+++ components/apimgt/org.wso2.carbon.apimgt.api/src/main/java/org/wso2/carbon/apimgt/api/model/URITemplate.java
@@ -55,25 +55,6 @@ public class URITemplate implements Serializable{
     private ApiOperationMapping apiOperationMapping = null;
     private BackendOperationMapping backendOperationMapping = null;
 
-    private String description;
-    private String shemaDefinition;
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getShemaDefinition() {
-        return shemaDefinition;
-    }
-
-    public void setShemaDefinition(String shemaDefinition) {
-        this.shemaDefinition = shemaDefinition;
-    }
-
     public ConditionGroupDTO[] getConditionGroups() {
         return conditionGroups;
     }
